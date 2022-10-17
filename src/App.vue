f8872f02f6cda98ecc8c345e94ef8513441c83f2
Peshang Hiwa <51766800+peshanghiwa@users.noreply.github.com>
2022-10-18 00:13:11 +0300

    created header

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
-<script setup></script>
+<script setup>
+import AppHeader from "./components/AppHeader.vue";
+</script>
 
 <template>
-  <div class="root-container"></div>
+  <main>
+    <AppHeader />
+    <section class="editor-body">
       <splitpanes vertical>
         <pane size="20" min-size="15" max-size="40">
           <span>1</span>
@@ -9,11 +13,19 @@
         <pane>
           <span>3</span>
         </pane>
+      </splitpanes>
+    </section>
+  </main>
 </template>
 
 <style scoped>
-.root-container {
+main {
   width: 100%;
   height: 100vh;
+}
+
+section.editor-body {
+  height: calc(100% - 50px);
+  width: 100%;
 }
 </style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,52 @@
+<script setup></script>
+
+<template>
+  <header>
+    <button class="btn btn-primary burger-button">
+      <img src="./../assets/menu.svg" height="20" width="20" alt="" />
+    </button>
+
+    <button class="btn btn-secondary action-button">
+      <img src="./../assets/run.svg" height="12" width="12" alt="" />
+      <span class="button-text"> Run </span>
+    </button>
+
+    <button class="btn btn-secondary action-button">
+      <img src="./../assets/stop.svg" height="12" width="12" alt="" />
+      <span class="button-text"> Kill </span>
+    </button>
+
+    <button class="btn btn-secondary action-button">
+      <img src="./../assets/save.svg" height="14" width="14" alt="" />
+      <span class="button-text"> Save </span>
+    </button>
+
+    <div class="spacer"></div>
+
+    <button class="btn btn-secondary action-button">
+      <img src="./../assets/horizontal.svg" height="20" width="20" alt="" />
+    </button>
+    <button class="btn btn-primary">Login</button>
+  </header>
+</template>
+
+<style scoped>
+header {
+  height: 50px;
+  width: 100%;
+  background-color: #2d4263;
+  display: flex;
+  align-items: center;
+  padding-inline: 15px;
+}
+.burger-button {
+  margin-inline-end: 20px;
+}
+.button-text {
+  margin-left: 5px;
+}
+
+.action-button {
+  margin-inline: 5px;
+}
+</style>
