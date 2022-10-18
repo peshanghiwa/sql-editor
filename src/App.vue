1e9c5b2b4594df73c5479ec5317e1c6e1ec332fc
Peshang Hiwa <51766800+peshanghiwa@users.noreply.github.com>
2022-10-18 10:12:08 +0300

    sidebar panes functionalities created

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,27 @@
 <script setup>
+import { ref } from "vue";
 import AppHeader from "./components/AppHeader.vue";
 import useSplitpaneConfig from "./composables/splitpaneConfig";
 
 const { isSidebarOpen, editorDirection } = useSplitpaneConfig();
+
+const sidebarPaneSizes = ref([94, 1.5, 1.5]);
+
+const onSidebarPaneButtonClick = (paneNumber) => {
+  sidebarPaneSizes.value.forEach((size, index) => {
+    if (index === paneNumber) {
+      sidebarPaneSizes.value[index] = 94;
+    } else {
+      sidebarPaneSizes.value[index] = 2.5;
+    }
+  });
+};
+
+const onSidebarPaneResized = (event) => {
+  sidebarPaneSizes.value.forEach((size, index) => {
+    sidebarPaneSizes.value[index] = event[index].size;
+  });
+};
 </script>
 
 <template>
@@ -11,15 +30,43 @@ const { isSidebarOpen, editorDirection } = useSplitpaneConfig();
     <section>
       <splitpanes vertical>
         <pane v-if="isSidebarOpen" size="20" min-size="15" max-size="40">
-          <span>1</span>
+          <splitpanes
+            @resized="onSidebarPaneResized"
+            :horizontal="editorDirection === 'horizontal'"
+          >
+            <pane :size="sidebarPaneSizes[0]" min-size="2.5">
+              <button
+                @click="onSidebarPaneButtonClick(0)"
+                class="splitpane-sidebar-buttons"
+              >
+                Tabs
+              </button>
+            </pane>
+            <pane :size="sidebarPaneSizes[1]" min-size="2.5">
+              <button
+                @click="onSidebarPaneButtonClick(1)"
+                class="splitpane-sidebar-buttons"
+              >
+                Saved Queries
+              </button>
+            </pane>
+            <pane :size="sidebarPaneSizes[2]" min-size="2.5">
+              <button
+                @click="onSidebarPaneButtonClick(2)"
+                class="splitpane-sidebar-buttons"
+              >
+                Tables
+              </button>
+            </pane>
+          </splitpanes>
         </pane>
         <pane>
           <splitpanes :horizontal="editorDirection === 'horizontal'">
-            <pane min-size="10">
-              <span>2</span>
+            <pane min-size="15">
+              <span>4</span>
             </pane>
-            <pane min-size="10">
-              <span>3</span>
+            <pane min-size="15">
+              <span>5</span>
             </pane>
           </splitpanes>
         </pane>
@@ -37,5 +84,12 @@ main {
 section {
   height: calc(100% - 50px);
   width: 100%;
+}
+
+.splitpane-sidebar-buttons {
+  width: 100%;
+  border: 1px solid #c84b31;
+  background-color: #c84b31;
+  cursor: pointer;
 }
 </style>
