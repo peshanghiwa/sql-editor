34cb13e2197a998a2c549e9c700a5becf8ab6cb7
Peshang Hiwa <51766800+peshanghiwa@users.noreply.github.com>
2022-10-18 10:53:55 +0300

    design issues fix

--- src/App.vue
+++ src/App.vue
@@ -31,7 +31,11 @@ const logPaneSize = ref(30);
       <splitpanes vertical>
         <pane v-if="isSidebarOpen" size="20" min-size="15" max-size="40">
           <splitpanes @resized="onSidebarPaneResized" horizontal="horizontal">
-            <pane :size="sidebarPaneSizes[0]" min-size="2.5">
+            <pane
+              class="sidebar-pane"
+              :size="sidebarPaneSizes[0]"
+              min-size="2.5"
+            >
               <button
                 @click="onSidebarPaneButtonClick(0)"
                 class="splitpane-sidebar-buttons"
@@ -39,7 +43,11 @@ const logPaneSize = ref(30);
                 Tabs
               </button>
             </pane>
-            <pane :size="sidebarPaneSizes[1]" min-size="2.5">
+            <pane
+              class="sidebar-pane"
+              :size="sidebarPaneSizes[1]"
+              min-size="2.5"
+            >
               <button
                 @click="onSidebarPaneButtonClick(1)"
                 class="splitpane-sidebar-buttons"
@@ -47,7 +55,11 @@ const logPaneSize = ref(30);
                 Saved Queries
               </button>
             </pane>
-            <pane :size="sidebarPaneSizes[2]" min-size="2.5">
+            <pane
+              class="sidebar-pane"
+              :size="sidebarPaneSizes[2]"
+              min-size="2.5"
+            >
               <button
                 @click="onSidebarPaneButtonClick(2)"
                 class="splitpane-sidebar-buttons"
@@ -99,8 +111,12 @@ section {
 
 .splitpane-sidebar-buttons {
   width: 100%;
-  border: 1px solid #c84b31;
+  border: 2px solid #c84b31;
   background-color: #c84b31;
   cursor: pointer;
+}
+
+.sidebar-pane {
+  background-color: #232323;
 }
 </style>
