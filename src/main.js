383d64cdee20bdd29483e11bffc79c6a24275742
Peshang Hiwa <51766800+peshanghiwa@users.noreply.github.com>
2022-10-21 15:53:01 +0300

    added vue tree view package

--- src/main.js
+++ src/main.js
@@ -3,15 +3,18 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import { Splitpanes, Pane } from "splitpanes";
 import { VueFinalModal } from "vue-final-modal";
+import Treeview from "vue3-treeview";
 
 // Styles
 import "splitpanes/dist/splitpanes.css";
 import "./style.css";
+import "vue3-treeview/dist/style.css";
 
 const app = createApp(App);
 
 app.component("splitpanes", Splitpanes);
 app.component("pane", Pane);
 app.component("vue-final-modal", VueFinalModal);
+app.component("tree-view", Treeview);
 
 app.mount("#app");
